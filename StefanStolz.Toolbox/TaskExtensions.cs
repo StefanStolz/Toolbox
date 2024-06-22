cb54fed3c65730b0686aef1bdfecd6ba5080ea77
Stefan Stolz <stefan.stolz@gmx.de>
2024-06-22 16:37:28 +0200

    Initial Version

---
+++ StefanStolz.Toolbox/TaskExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace StefanStolz.Toolbox;
+
+public static class TaskExtensions
+{
+    public static async void SafeFireAndForget(this Task task, Action<Exception> onException)
+    {
+        try
+        {
+            await task;
+        }
+        catch (Exception exception)
+        {
+            onException(exception);
+        }
+    }
+}
